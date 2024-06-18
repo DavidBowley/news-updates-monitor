e95efee1c936d3a6ff9f33e57411da6977f77272
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2024-06-18 21:26:55 +0100

    Proof of concept for extracting headline and article body paragraphs

--- news_updates_monitor.py
+++ news_updates_monitor.py
@@ -1,8 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 
-url = 'https://www.bbc.co.uk/news/articles/cg33v21weg3o'
+url = 'https://www.bbc.co.uk/news/articles/cw00rgq24xvo'
 page = requests.get(url)
 
-soup = BeautifulSoup(page.text, 'lxml')
-print(soup.title)
+soup = bs4.BeautifulSoup(page.text, 'lxml')
+headline = soup.h1.string
+
+text_block_divs = soup.find_all('div', attrs={'data-component': 'text-block'})
+
+body_paragraphs = ''
+
+for div in text_block_divs:
+    for p in div.find_all('p'):
+        # Delete class attribute from each parent <p>
+        del p['class']
+        # Do the same for each descendant of each <p> that is a Tag object (e.g. <a href> and <b>)
+        for tag in p.descendants:
+            if isinstance(tag, bs4.element.Tag):
+                del tag['class']
+        body_paragraphs += str(p)
+
+print('***Article headline***\n', headline)
+print('\n***Article body***\n', body_paragraphs)
+
