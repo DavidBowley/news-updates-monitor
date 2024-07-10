b9a3629452eb9d48300ea3ace6f39a6e8f4f801f
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2024-07-10 21:56:40 +0100

    Added Byline parsing logic proof of concept

--- news_updates_monitor.py
+++ news_updates_monitor.py
@@ -2,6 +2,9 @@ import requests
 import bs4
 
 url = 'https://www.bbc.co.uk/news/articles/cw00rgq24xvo'
+# url = 'https://www.bbc.co.uk/news/articles/c4ngk17zzkpo'
+# url = 'https://www.bbc.co.uk/news/articles/cq5xel42801o'
+# url ='https://www.bbc.co.uk/news/articles/cl4y8ljjexro'
 page = requests.get(url)
 # For some reason Requests is auto-detecting the wrong encoding so we're getting Mojibakes everywhere
 # Hard-coding as utf-8 shouldn't cause issues unless BBC change it for random pages which is unlikely
@@ -26,3 +29,14 @@ for div in text_block_divs:
 
 print('***Article headline***\n', headline)
 print('\n***Article body***\n', body_paragraphs)
+print('\n***Byline***')
+
+byline_block_div = soup.find('div', attrs={'data-component': 'byline-block'})
+if byline_block_div is None:
+    # There is no byline-block present (some articles don't have one)
+    print('There is no parsable byline data present in the article')
+else:
+    # The data is too inconsistent to create a reliable mapping (e.g. Author, Job Title, etc.) as different authors don't all have the same types of data
+    # It is probably possible with some work, but as the main focus of the project is comparing article headlines/body this will be left as a list of strings for now
+    for string in byline_block_div.strings:
+        print(string)
