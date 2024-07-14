4fb985451290e0225d046529a816693750ca42a1
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2024-07-14 18:07:11 +0100

    Setup Logging to both conosle and a log file

--- news_updates_monitor.py
+++ news_updates_monitor.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 from datetime import datetime
+import logging
 
 class Article():
     """ An Article object represents one individual BBC News article
@@ -57,6 +58,9 @@ class Article():
 
     def parse_headline(self):
         self.headline = self.soup.h1.string
+        # Parse error handling
+        if self.headline is None:
+            logger.error('Parse Error: self.soup.h1.string returned None, the Headline could not be parsed')
     
     def parse_body(self):
         text_block_divs = self.soup.find_all('div', attrs={'data-component': 'text-block'})
@@ -104,13 +108,12 @@ def testing_Article_class():
     test_article.fetch_HTML()
     test_article.parse_all()
 
-    print('***Article headline***\n' + test_article.headline)
-    print('\n***Article body***\n' + test_article.body)
+    print('***Article headline***')
+    print(test_article.headline)
+    print('\n***Article body***')
+    print(test_article.body)
     print('\n***Byline***')
-
-    for string in test_article.byline:
-        print(string)
-
+    print(test_article.byline)
     print('\n***Date Info***')
     print(test_article.timestamp)
 
@@ -136,7 +139,33 @@ def get_news_urls():
     news_urls = list(set(news_urls))
     return news_urls
     
-news_links = get_news_urls()
-for link in news_links:
-    print(link)
-print(len(news_links))
+
+# Create a logger
+logger = logging.getLogger('__name__')
+logger.setLevel(logging.DEBUG)
+
+# Create a formatter to define the log format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+
+# Create a file handler to write logs to a file
+file_handler = logging.FileHandler('debug.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+# Create a stream handler to print logs to the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# Test...
+logger.debug('This is a debug message')
+logger.info('This is an info message')
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+
+
+# testing_Article_class()
