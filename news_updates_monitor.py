0a0d2df32c2ef679a31e98fe73154999b45596c3
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2024-07-25 17:59:06 +0100

    Large reorganising of the code - moving debug functions and sorting to make it easier to use

--- news_updates_monitor.py
+++ news_updates_monitor.py
@@ -161,13 +161,14 @@ class Article():
         """
         logger.debug(
             '\n***URL***\n%s' +
+            '\n\n***Fetched Timestamp***\n%s' +
             '\n\n***Parse Errors***\n%s' +
             '\n\n***Headline***\n%s' +
             '\n\n***Body***\n%s' +
             '\n\n***Byline***\n%s' +
             '\n\n***Timestamp***\n%s' +
             '\n\n',
-            self.url, self.parsed['parse_errors'], self.parsed['headline'],
+            self.url, self.fetched_timestamp, self.parsed['parse_errors'], self.parsed['headline'],
             self.parsed['body'], self.parsed['byline'], self.parsed['timestamp']
             )
 
@@ -225,6 +226,30 @@ class TimeoutHTTPAdapter(HTTPAdapter):
         return super().send(request, **kwargs)
 
 
+""" 
+    Start of Testing / Debug functions
+"""
+
+def testing_print_latest_news():
+    """ Test function to debug_log print the latest news """
+    articles = get_latest_news()
+    for article in articles:
+        article.debug_log_print()
+
+def testing_exceptions():
+    """ Test function to make sure Timeout, Connection and other exceptions
+        are working via requests_throttler
+    """
+    urls = [
+    'https://github.com/kennethreitz/requests/issues/1236',
+    'https://www.github.com',
+    'http://fjfklsfjksjdoijfkjsldf.com/',
+    'https://www.yahoo.co.uk',
+    ]
+    articles = urls_to_parsed_articles(urls, delay=5)
+    for article in articles:
+        article.debug_log_print()
+
 def testing_article_class():
     """ Test function for the Article class instance methods """
     # url = 'https://www.bbc.co.uk/news/articles/cw00rgq24xvo'
@@ -243,16 +268,126 @@ def testing_article_class():
     test_article.parse_all()
     test_article.debug_log_print()
 
-def debug_file_to_article_object(filename):
+def debug_file_to_article_object(url, filename):
     """ Debugging function: turn an offline file into an article object for testing purposes
         instead of fetching a live URL
         Pulls the HTML into the self.raw_html attribute
-        URL is not used and set to 'DEBUG'
     """
-    debug_article = Article('DEBUG: no URL as created from offline file')
+    debug_article = Article(url)
     with open(filename, encoding='utf-8') as my_file:
         debug_article.raw_html = my_file.read()
     return debug_article
+
+def testing_shelves():
+    """ Test function """
+    with shelve.open('testing_db') as db:
+        # db['testing'] = 'This is a test'
+        temp = db['testing']
+        print(temp)
+
+def testing_build_dummy_db():
+    """ Test function: builds a dummy database with IDs ready for testing """
+    with shelve.open('articles_db') as db:
+        for i in range(10, 0, -1):
+            db[str(i)] = None
+
+def testing_print_db():
+    """ Test function: prints out the article objects from persistent storage """
+    with shelve.open('articles_db') as db:
+        keys = list(db.keys())
+        keys.sort(key=int)
+        for key in keys:
+            print(key, db[key])
+
+def debug_table(attrs, parsed):
+    """ Builds a data table inside a HTML file with all the information from the article objects we
+        want to see.
+        Currently pulls from template files in /debug_table/ (which is not part of the Git repo)
+        but only using it for debugging so far.
+        attrs = list of strings; must only include known Article object attributes (but NOT parsed)
+        parsed = dictionary keys as per article.parsed[...] to specify specific parsed info
+        The ID will always be output as the first column regardless of other details requested
+    """
+    # pylint: disable=too-many-locals
+    # Agree with pylint that this function is too complex, however it's purely for internal
+    # debugging and won't be in the final version in this form. If I do decide to reuse parts of
+    # this (e.g. to make a dashboard) then I will definitely simplify it and remove some of the
+    # variables.
+    with open('debug_table/top.html', encoding='utf-8') as f:
+        template_start = f.read()
+    with open('debug_table/bottom.html', encoding='utf-8') as f:
+        template_end = f.read()
+    data_table_start = ''
+    start_indent = 4
+    data_table_start += (
+        indent(start_indent) + '<table>\n' +
+        indent(start_indent + 2) + '<caption>' + 'Debug Table' + '</caption>\n'
+        )
+    data_table_start += indent(start_indent+2) + '<tr>\n'
+    data_table_start += indent(start_indent+4) + '<th>' + 'ID' + '</th>\n'
+    data_table_start += indent(start_indent+4) + '<th>' + 'Snapshot' + '</th>\n'
+    for th in attrs:
+        data_table_start += indent(start_indent+4) + '<th>' + th + '</th>\n'
+    for th in parsed:
+        data_table_start += indent(start_indent+4) + '<th>' + th + '</th>\n'
+    data_table_start += indent(start_indent+2) + '</tr>'
+    # Overwrite existing HTML file if it exists
+    with open('debug_table/debug_table.html', 'w', encoding='utf-8') as f:
+        f.write(template_start)
+        f.write('\n\n' + data_table_start + '\n\n')
+    # Start outputing the data rows
+    with shelve.open('articles_db') as db:
+        ids = list(db.keys())
+        ids.sort(key=int)
+        for _id in ids:
+            article_list = db[_id]
+            n = 0
+            for article in article_list:
+                article_dict = article.__dict__
+                data_row = ''
+                data_row += indent(start_indent+2) + '<tr>\n'
+                data_row += indent(start_indent+4) + '<td>' + _id + '</td>\n'
+                data_row += indent(start_indent+4) + '<td>' + str(n) + '</td>\n'
+                for attr in  attrs:
+                    data_row += (
+                        indent(start_indent+4) + '<td>' + str(article_dict[attr]) + '</td>\n'
+                        )
+                for item in parsed:
+                    data_row += (
+                        indent(start_indent+4) + '<td>' + str(article.parsed[item]) + '</td>\n'
+                        )
+                data_row += indent(start_indent+2) + '</tr>'
+                # Append to existing HTML file
+                with open('debug_table/debug_table.html', 'a', encoding='utf-8') as f:
+                    f.write(data_row + '\n\n')
+                n += 1
+        data_table_end = indent(start_indent) + '</table>'
+        # End the table and finalise file template - append to existing HTML file
+        with open('debug_table/debug_table.html', 'a', encoding='utf-8') as f:
+            f.write(data_table_end + '\n\n')
+            f.write(template_end)
+
+def indent(spaces):
+    """ Returns a string matching the number of spaces needed for the indent
+        spaces = integer
+    """
+    return ' ' * spaces
+
+def testing_article_comparison():
+    article1 = debug_file_to_article_object(url='https://www.bbc.co.uk/news/articles/cjqep1ew419o', filename='test_file.html')
+    article1.fetched_timestamp = datetime.now(timezone.utc)
+    article1.parse_all()
+    
+    article2 = debug_file_to_article_object(url='https://www.bbc.co.uk/news/articles/cjqep1ew419o', filename='test_file2.html')
+    article2.fetched_timestamp = datetime.now(timezone.utc)
+    article2.parse_all()
+    
+    print(article1.parsed == article2.parsed)
+
+""" 
+    End of Testing / Debug functions
+"""
+
 
 def request_html(url):
     """ Helper function for using requests to get the HTML """
@@ -273,6 +408,36 @@ def request_html(url):
             url, e, type(e)
             )
         return None
+
+
+
+def testing_store_articles():
+    """ Test function to get the latest news and then store in persistent storage """
+    articles = get_latest_news()
+    for article in articles:
+        article.store()
+
+def get_latest_news():
+    """ Parses the latest news articles from the BBC news homepage
+        Returns a list of article objects
+    """
+    # DEBUG: only 2 hard-coded URLs
+    #urls = [
+    #'https://www.bbc.co.uk/news/articles/cw00rgq24xvo',
+    #'https://www.bbc.co.uk/news/articles/c4ngk17zzkpo'
+    #]
+
+    # DEBUG: only 1 hard-coded URL
+    # urls = ['https://www.bbc.co.uk/news/articles/cw00rgq24xvo']
+
+    urls = get_news_urls(debug=5)
+    # As get_news_urls() has just been called, there has already been a HTTP request within the
+    # last few milliseconds. It's possible the first request of the throttler will be sent too
+    # close to the homepage scrape request - so we delay to avoid this
+    time.sleep(2)
+    articles = urls_to_parsed_articles(urls, delay=5)
+    return articles
+
 
 def get_news_urls(debug=None):
     """ Extracts all the news article URLs from the BBC Homepage
@@ -355,150 +520,6 @@ def urls_to_parsed_articles(urls, delay):
 
     return res
 
-def get_latest_news():
-    """ Parses the latest news articles from the BBC news homepage
-        Returns a list of article objects
-    """
-    # DEBUG: only 2 hard-coded URLs
-    #urls = [
-    #'https://www.bbc.co.uk/news/articles/cw00rgq24xvo',
-    #'https://www.bbc.co.uk/news/articles/c4ngk17zzkpo'
-    #]
-
-    # DEBUG: only 1 hard-coded URL
-    # urls = ['https://www.bbc.co.uk/news/articles/cw00rgq24xvo']
-
-    urls = get_news_urls(debug=5)
-    # As get_news_urls() has just been called, there has already been a HTTP request within the
-    # last few milliseconds. It's possible the first request of the throttler will be sent too
-    # close to the homepage scrape request - so we delay to avoid this
-    time.sleep(2)
-    articles = urls_to_parsed_articles(urls, delay=5)
-    return articles
-
-def testing_print_latest_news():
-    """ Test function to debug_log print the latest news """
-    articles = get_latest_news()
-    for article in articles:
-        article.debug_log_print()
-
-def testing_exceptions():
-    """ Test function to make sure Timeout, Connection and other exceptions
-        are working via requests_throttler
-    """
-    urls = [
-    'https://github.com/kennethreitz/requests/issues/1236',
-    'https://www.github.com',
-    'http://fjfklsfjksjdoijfkjsldf.com/',
-    'https://www.yahoo.co.uk',
-    ]
-    articles = urls_to_parsed_articles(urls, delay=5)
-    for article in articles:
-        article.debug_log_print()
-
-def testing_store_articles():
-    """ Test function to get the latest news and then store in persistent storage """
-    articles = get_latest_news()
-    for article in articles:
-        article.store()
-
-def debug_table(attrs, parsed):
-    """ Builds a data table inside a HTML file with all the information from the article objects we
-        want to see.
-        Currently pulls from template files in /debug_table/ (which is not part of the Git repo)
-        but only using it for debugging so far.
-        attrs = list of strings; must only include known Article object attributes (but NOT parsed)
-        parsed = dictionary keys as per article.parsed[...] to specify specific parsed info
-        The ID will always be output as the first column regardless of other details requested
-    """
-    # pylint: disable=too-many-locals
-    # Agree with pylint that this function is too complex, however it's purely for internal
-    # debugging and won't be in the final version in this form. If I do decide to reuse parts of
-    # this (e.g. to make a dashboard) then I will definitely simplify it and remove some of the
-    # variables.
-    with open('debug_table/top.html', encoding='utf-8') as f:
-        template_start = f.read()
-    with open('debug_table/bottom.html', encoding='utf-8') as f:
-        template_end = f.read()
-    data_table_start = ''
-    start_indent = 4
-    data_table_start += (
-        indent(start_indent) + '<table>\n' +
-        indent(start_indent + 2) + '<caption>' + 'Debug Table' + '</caption>\n'
-        )
-    data_table_start += indent(start_indent+2) + '<tr>\n'
-    data_table_start += indent(start_indent+4) + '<th>' + 'ID' + '</th>\n'
-    data_table_start += indent(start_indent+4) + '<th>' + 'Snapshot' + '</th>\n'
-    for th in attrs:
-        data_table_start += indent(start_indent+4) + '<th>' + th + '</th>\n'
-    for th in parsed:
-        data_table_start += indent(start_indent+4) + '<th>' + th + '</th>\n'
-    data_table_start += indent(start_indent+2) + '</tr>'
-    # Overwrite existing HTML file if it exists
-    with open('debug_table/debug_table.html', 'w', encoding='utf-8') as f:
-        f.write(template_start)
-        f.write('\n\n' + data_table_start + '\n\n')
-    # Start outputing the data rows
-    with shelve.open('articles_db') as db:
-        ids = list(db.keys())
-        ids.sort(key=int)
-        for _id in ids:
-            article_list = db[_id]
-            n = 0
-            for article in article_list:
-                article_dict = article.__dict__
-                data_row = ''
-                data_row += indent(start_indent+2) + '<tr>\n'
-                data_row += indent(start_indent+4) + '<td>' + _id + '</td>\n'
-                data_row += indent(start_indent+4) + '<td>' + str(n) + '</td>\n'
-                for attr in  attrs:
-                    data_row += (
-                        indent(start_indent+4) + '<td>' + str(article_dict[attr]) + '</td>\n'
-                        )
-                for item in parsed:
-                    data_row += (
-                        indent(start_indent+4) + '<td>' + str(article.parsed[item]) + '</td>\n'
-                        )
-                data_row += indent(start_indent+2) + '</tr>'
-                # Append to existing HTML file
-                with open('debug_table/debug_table.html', 'a', encoding='utf-8') as f:
-                    f.write(data_row + '\n\n')
-                n += 1
-        data_table_end = indent(start_indent) + '</table>'
-        # End the table and finalise file template - append to existing HTML file
-        with open('debug_table/debug_table.html', 'a', encoding='utf-8') as f:
-            f.write(data_table_end + '\n\n')
-            f.write(template_end)
-
-def indent(spaces):
-    """ Returns a string matching the number of spaces needed for the indent
-        spaces = integer
-    """
-    return ' ' * spaces
-
-
-
-def testing_shelves():
-    """ Test function """
-    with shelve.open('testing_db') as db:
-        # db['testing'] = 'This is a test'
-        temp = db['testing']
-        print(temp)
-
-def testing_build_dummy_db():
-    """ Test function: builds a dummy database with IDs ready for testing """
-    with shelve.open('articles_db') as db:
-        for i in range(10, 0, -1):
-            db[str(i)] = None
-
-def testing_print_db():
-    """ Test function: prints out the article objects from persistent storage """
-    with shelve.open('articles_db') as db:
-        keys = list(db.keys())
-        keys.sort(key=int)
-        for key in keys:
-            print(key, db[key])
-
 
 
 if __name__ == '__main__':
@@ -535,3 +556,5 @@ if __name__ == '__main__':
     # testing_store_articles()
 
     debug_table(attrs = ['fetched_timestamp'], parsed=['parse_errors', 'headline', 'body', 'byline', 'timestamp'])
+
+    
